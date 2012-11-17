48aae7c300c57d7df1abd484ee4140e3d404e44d
viettran <viettran@viettran>
2012-11-17 16:31:40 -0500

    Complete

--- src/model/JottoModel.java
+++ src/model/JottoModel.java
@@ -1,14 +1,123 @@
 package model;
 
+import java.net.*;
+import java.util.ArrayList;
+import java.io.*;
+
+import javax.swing.table.AbstractTableModel;
+
 /**
- * // TODO Write specifications for your JottoModel!
+ * JottoModel is the data model for GUI JTable Have an internal array of
+ * returnMsg Fully implement AbstractTableModel data type with its methods
+ * (getColumnCount, getRowCount, ...) Have three columns ("Guess", "Common",
+ * "Correct") For each valid cell position must have an appropriate value for
+ * each use case
  */
-public class JottoModel {
-	
+@SuppressWarnings("serial")
+public class JottoModel extends AbstractTableModel {
+	/** Three columns for the table */
+	private String[] columnNames = { "Guess", "Common", "Correct" };
+	/** Arrasy to store the return message from the server */
+
+	private ArrayList<ReturnMsg> data = new ArrayList<ReturnMsg>();
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		return data.size();
+	}
+
 	/**
-	 * // TODO Write specifications for the makeGuess function.
+	 * return the table value at row, col
+	 * 
 	 */
-	public void makeGuess(String guess) {
-		// TODO Problem 1
+	public Object getValueAt(int row, int col) {
+		ReturnMsg msg = data.get(row);
+		if (col == 0) {
+			return msg.getGuess();
+		} else {
+			if (msg.isDone()) {
+				if (msg.isError()) {
+					if (col == 1) {
+						return msg.toString();
+					}
+					return "";
+				} else {
+					int common = msg.getCommon();
+					int correct = msg.getCorrect();
+					if (common == 5 && correct == 5) {
+						if (col == 1)
+							return "You win!The secret word was "
+									+ msg.getGuess() + "!";
+						return "";
+					} else {
+						if (col == 1)
+							return common;
+						return correct;
+					}
+				}
+			} else {
+				if (col == 1)
+					return "waiting for result from server...";
+				return "";
+			}
+		}
+	}
+
+	/**
+	 * Add the return message to the table
+	 * 
+	 * @return int: current size of the array of message
+	 * @param returnMsg
+	 *            : message returned from the server modifes: redraw table
+	 * */
+	public int addRow(ReturnMsg msg) {
+		data.add(msg);
+		this.fireTableDataChanged();
+		return data.size() - 1;
+	}
+
+	/**
+	 * Set the value of a specific rowNo to the return message
+	 * 
+	 * @param returnMsg
+	 *            : return message from server
+	 * @param int: row Number modifes: redraw table
+	 * */
+	public void setRow(ReturnMsg msg, int rowNo) {
+		data.set(rowNo, msg);
+		this.fireTableDataChanged();
+	}
+
+	/**
+	 * Clear all rows of the table modifes: redraw table
+	 * */
+	public void clearRow() {
+		data.clear();
+		this.fireTableDataChanged();
+	}
+
+	/**
+	 * make a string guess to the server, read back the reply and return it
+	 * requires guess, id is not null
+	 * 
+	 * @param puzzleID
+	 *            : the ID of the puzzle
+	 * @param guess
+	 *            : the guess string from the user
+	 * @return returnMsg from the server
+	 * @throws IOException
+	 */
+	public static ReturnMsg makeGuess(int puzzleID, String guess)
+			throws IOException {
+		String url = "http://6.005.scripts.mit.edu//jotto.py?puzzle="
+				+ puzzleID + "&guess=" + guess;
+		URL server = new URL(url);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				server.openStream()));
+		String input = in.readLine();
+		return ReturnMsg.parseMsg(guess, input);
 	}
 }
--- src/ui/JottoGUI.java
+++ src/ui/JottoGUI.java
@@ -1,5 +1,11 @@
 package ui;
 
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,30 +13,215 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
+import model.JottoModel;
+import model.ReturnMsg;
+import model.Worker;
+
 /**
- * // TODO Write specifications for your JottoGUI class.
+ * JottoGUI class Have: A button to get new puzzle number A textfield for user
+ * to input new puzzle number A label to display puzzle number
  */
+@SuppressWarnings("serial")
 public class JottoGUI extends JFrame {
 
+	/**
+	 * Button to generate new puzzle
+	 */
 	private JButton newPuzzleButton;
+	/**
+	 * TextField to enter new puzzle from users
+	 */
 	private JTextField newPuzzleNumber;
+	/**
+	 * Label to display puzzle number
+	 */
 	private JLabel puzzleNumber;
+	/**
+	 * TextField to enter a guess
+	 */
 	private JTextField guess;
+	/**
+	 * Table to display guesses
+	 */
 	private JTable guessTable;
+	/**
+	 * General label
+	 */
+	private JLabel guessLabel;
+	/**
+	 * Data model for the table
+	 */
+	private JottoModel model;
+	/**
+	 * length limit of the input guess
+	 */
+	private int GUESS_LIMIT = 30;
+	/**
+	 * Puzzle ID
+	 */
+	private static int puzzleID = 16952;
 
+	/**
+	 * setting the layout for the GUI 
+	 * Layout consists of three Group Layout which make up puzzle number group,
+	 * guess group and table group
+	 */
+	public void setLayout() {
+		Container cp = this.getContentPane();
+		GroupLayout grpLayout = new GroupLayout(cp);
+		cp.setLayout(grpLayout);
+		grpLayout.setAutoCreateGaps(true);
+		grpLayout.setAutoCreateContainerGaps(true);
+
+		GroupLayout.SequentialGroup horSeqGrp = grpLayout
+				.createSequentialGroup();
+		GroupLayout.SequentialGroup verSeqGrp = grpLayout
+				.createSequentialGroup();
+
+		GroupLayout.ParallelGroup horPaGrp1 = grpLayout.createParallelGroup();
+
+		GroupLayout.SequentialGroup horSeqGrp1 = grpLayout
+				.createSequentialGroup();
+		GroupLayout.SequentialGroup horSeqGrp2 = grpLayout
+				.createSequentialGroup();
+		GroupLayout.SequentialGroup horSeqGrp3 = grpLayout
+				.createSequentialGroup();
+
+		horSeqGrp1.addComponent(puzzleNumber);
+		horSeqGrp1.addComponent(newPuzzleButton);
+		horSeqGrp1.addComponent(newPuzzleNumber);
+		horPaGrp1.addGroup(horSeqGrp1);
+
+		horSeqGrp2.addComponent(guessLabel);
+		horSeqGrp2.addComponent(guess);
+		horPaGrp1.addGroup(horSeqGrp2);
+
+		horSeqGrp3.addComponent(guessTable);
+		horPaGrp1.addGroup(horSeqGrp3);
+
+		horSeqGrp.addGroup(horPaGrp1);
+
+		GroupLayout.ParallelGroup row1 = grpLayout
+				.createParallelGroup(GroupLayout.Alignment.BASELINE);
+		GroupLayout.ParallelGroup row2 = grpLayout.createParallelGroup();
+		GroupLayout.ParallelGroup row3 = grpLayout.createParallelGroup();
+
+		row1.addComponent(puzzleNumber);
+		row1.addComponent(newPuzzleButton);
+		row1.addComponent(newPuzzleNumber);
+
+		row2.addComponent(guessLabel);
+		row2.addComponent(guess);
+
+		row3.addComponent(guessTable);
+
+		verSeqGrp.addGroup(row1);
+		verSeqGrp.addGroup(row2);
+		verSeqGrp.addGroup(row3);
+
+		grpLayout.setHorizontalGroup(horSeqGrp);
+		grpLayout.setVerticalGroup(verSeqGrp);
+	}
+
+	/**
+	 * Main GUI of the game
+	 */
 	public JottoGUI() {
+		super("Simple Jotto GUI");
+		model = new JottoModel();
 		newPuzzleButton = new JButton();
 		newPuzzleButton.setName("newPuzzleButton");
+		newPuzzleButton.setText("New Puzzle");
+
 		newPuzzleNumber = new JTextField();
 		newPuzzleNumber.setName("newPuzzleNumber");
+
 		puzzleNumber = new JLabel();
 		puzzleNumber.setName("puzzleNumber");
-		guess = new JTextField();
+		puzzleNumber.setText("Puzzle #16952");// Any random number initially is
+												// fine
+
+		guessLabel = new JLabel();
+		guessLabel.setName("guessLabel");
+		guessLabel.setText("Type a guess here:");
+
+		guess = new JTextField(GUESS_LIMIT);// limit to 30 chacter long for the
+											// guess
 		guess.setName("guess");
-		guessTable = new JTable();
+		guessTable = new JTable(model);
 		guessTable.setName("guessTable");
-		
-		// TODO Problem 3, 4, and 5
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setLayout();
+
+		// adding action listener for the guess textbox
+		guess.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getPuzzleResult();
+			}
+		});
+
+		// adding action listenr for the newPuzzle button
+		newPuzzleButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setNewPuzzle();
+			}
+
+		});
+		this.pack();
+	}
+
+	/**
+	 * Get the result from the server and reset guess text field add the return
+	 * message to table in correct format modifies: JottoModel by add one row to
+	 * it, guessField resets to empty string
+	 */
+	private void getPuzzleResult() {
+		String guessStr = guess.getText();
+		int rowNo = model.addRow(new ReturnMsg(guessStr));
+		new Worker(model, puzzleID, guessStr, rowNo).execute();
+		guess.setText("");
+		pack();
+	}
+
+	/**
+	 * Set the puzzle number to retrieve puzzle from the server if the user
+	 * inputs incorrect format puzzle number, puzzle number will be randomly
+	 * generated. The puzzle number will be displayed on the GUI modifies:
+	 * modelTable will be reset to ihe initial state
+	 */
+	private void setNewPuzzle() {
+		try {
+			int newPuzzleNo = Integer.parseInt(newPuzzleNumber.getText());
+			if (newPuzzleNo != puzzleID) {
+				puzzleID = newPuzzleNo;
+				puzzleNumber.setText("Puzzle #" + puzzleID);
+				model.clearRow();
+				pack();
+			} else {
+				return;
+			}
+		} catch (NumberFormatException ex) {
+			Random r = new Random();
+			puzzleID = r.nextInt(10000);
+			newPuzzleNumber.setText(puzzleID + "");
+			puzzleNumber.setText("Puzzle #" + puzzleID);
+			model.clearRow();
+			pack();
+		}
+
+	}
+
+	/**
+	 * 
+	 * @return: the current puzzle ID of the game
+	 */
+	public static int getPuzzleID() {
+		return puzzleID;
 	}
 
 	public static void main(final String[] args) {
